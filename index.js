2c099885c7e49794f963b01f5956cf569c92638c
iaouvaeiru <leonkind@gmail.com>
2023-04-18 22:48:14 -0500

    basic setup in index.js

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-const bigBomba = [];
-bigBomba.push(3);
-console.log(bigBomba);
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
+
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// When the client is ready, run this code (only once)
+// We use 'c' for the event parameter to keep it separate from the already defined 'client'
+client.once(Events.ClientReady, c => {
+	console.log(`Banhammer Ready! ${c.user.tag}`);
+});
+
+// Log in to Discord with your client's token
+client.login(token);
